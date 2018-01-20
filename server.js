fc308bce806f1dcd68655749bc064330ec279ee9
Sharareh Shafiei <sharareh@Shararehs-MacBook-Pro.local>
2018-01-20 10:19:26 -0800

    mongoos

--- server.js
+++ server.js
@@ -30,6 +30,14 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 var db = mongoose.connection;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
 db.on("error", function(error) {
   console.log("Mongoose Error: ", error);
